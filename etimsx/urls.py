e8b07a63c149f8a9d3677a9de743239c660fa607
Lyonsmasawa <lyonsmasawa@gmail.com>
2024-12-15 08:57:59 -0800

    base files

--- etimsx/urls.py
+++ etimsx/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,7 @@ urlpatterns = [
     path('api/inventory/', include('inventory.urls')),        # inventory URLs
     path('api/sales_items/', include('sales_items.urls')),    # sales_items URLs
 ]
+
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
